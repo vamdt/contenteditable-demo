ab22207f31ca1da6cf8d8fed68df6cd342793734
vamdt <liang6688267@gmail.com>
2014-04-12 12:36:27 +0800

    Set caret when focus editable

--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
 (function(root) {
+  var editable, document = root.document;
 
+
+  function fire_event(elemnet, event) {
+    var evt;
+    if (document.createEventObject) {
+      evt = document.createEventObject();
+      return elemnet.fireEvent('on'+event, evt);
+    } else {
+      evt = document.createEvent('HTMLEvents');
+      evt.initEvent(event, true, true);
+      return !elemnet.dispatchEvent(evt);
+    }
+  }
+
+
+  function set_caret_position(elem, event) {
+      var sel = root.getSelection(),
+          range = document.createRange(),
+          last_child = elem.childNodes[elem.childNodes.length-1];
+      range.setStart(last_child, 0);
+      range.collapse(true);
+      sel.removeAllRanges();
+      sel.addRange(range);
+  }
+
+  editable = document.querySelector('div[contenteditable="true"]');
+  editable.addEventListener('focus', function(event) {
+    set_caret_position(this, event);
+  });
   
-  
+  fire_event(editable, 'focus');
+
 })(window);
