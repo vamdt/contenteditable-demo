8fe2e739ee4666866b4a0515508b3fe081dc9966
vamdt <liang6688267@gmail.com>
2014-04-12 13:12:42 +0800

    Dynamic generate mention div

--- src/js/main.js
+++ src/js/main.js
@@ -19,13 +19,18 @@
       var sel = root.getSelection(),
           range = document.createRange(),
           last_child = elem.childNodes[elem.childNodes.length-1];
-      range.setStart(last_child, 0);
+      range.setStart(last_child, 1);
       range.collapse(true);
       sel.removeAllRanges();
       sel.addRange(range);
   }
 
   editable = document.querySelector('div[contenteditable="true"]');
+  console.log(editable.innerHTML);
+  editable.innerHTML = '<button class="mention" contenteditable="false">' +
+                          '<i>You</i> : </button>' +
+                        '&nbsp;';
+  
   editable.addEventListener('focus', function(event) {
     set_caret_position(this, event);
   });
